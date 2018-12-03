7f8b9049ef196a0a744670555b7e5c2adea308ca
huong1999 <37472308+huong1999@users.noreply.github.com>
2018-12-03 17:39:09 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,52 +1,41 @@
 package com.company;
 
-import java.util.ArrayList;
-
-public class Main implements Sort{
+public class Main {
 
     public static void main(String[] args) {
-        System.out.println("-Truoc khi sap xep : ");
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(1);
-        arr.add(3);
-        arr.add(2);
-        arr.add(10);
-        arr.add(7);
-        for(int i = 0; i < arr.size(); i++) {
-            System.out.print(arr.get(i) + "\t");
-        }
-        System.out.println();
+        Human james = new Human("James","19/2/1965","Nam");
+        Human hana = new Human("Hana","30/7/1975","Nữ");
+        Human ryan = new Human("Ryan","21/5/1990","Nam");       //kết hôn vs jeni
+        Human kai = new Human("Kai","13/6/1995","Nam");         //độc thân
+        Human jenifer = new Human("Jenifer","12/7/1996","Nữ");
+        Human jackson = new Human("Jackson","19/1/2005","Nam");
+        Human Lina = new Human("Lina","5/8/2008","Nữ");
+        Human Tracy = new Human("Tracy","31/12/2010","Nữ");
+        Human taylor = new Human("Taylor","1/12/2011","Nữ");
 
-        Main mt = new Main();
-        mt.sortArray(arr);
-        System.out.println("-Sau khi sap xep : ");
-        for(int i = 0; i < arr.size(); i++) {
-            System.out.print(arr.get(i) + "\t");
+        james.getMarried(hana);
+        james.addChild(ryan);
+        james.addChild(kai);
+        kai.getMarried(jenifer);
+        kai.addChild(jackson);
+        kai.addChild(Lina);
+        kai.addChild(Tracy);
+        kai.addChild(taylor);
+        //moi thu deu duoc tinh tu to tien
+        //tim nguoi doc than trong pha he
+        System.out.println("-Nhung nguoi doc than la:");
+        for(int i =0; i < james.findNotMarriedChildren().size(); i++) {
+            System.out.println("\t"+james.findNotMarriedChildren().get(i));
+        }
+        //tim ngoui co hai con trong pha he
+        System.out.println("-Cac cap co hai con la:");
+        for(int i =0; i < james.findHasTwoChildren().size(); i++) {
+            System.out.println("\t"+james.findHasTwoChildren().get(i));
+        }
+        //tim the he moi nhat
+        System.out.println("-The he moi nhat la:");
+        for(int i =0; i < james.getNewestGeneration().size(); i++) {
+            System.out.println("\t"+james.getNewestGeneration().get(i));
         }
     }
-
-    @Override
-    public <T extends Comparable> ArrayList<T> sortArray(ArrayList<T> arr) {
-        if(arr == null) return null;
-        else {
-            boolean continueSort = true;
-            while(continueSort) {
-                continueSort = false;
-                for(int i = 0; i < arr.size()-1; i++) {
-                    if(arr.get(i).compareTo(arr.get(i+1)) >= 0) {
-                        //lay phan tu thu i luu vao element
-                        T element = arr.get(i);
-                        //xoa phan tu thu i do
-                        arr.remove(i);
-                        //them phan tu element vao vi tri i+1
-                        arr.add(i+1, element);
-                        //van con phan tu ko dung vi tri => tiep tuc sap xep
-                        continueSort = true;
-                    }
-                }
-            }
-        }
-        return arr;
-    }
-
 }
